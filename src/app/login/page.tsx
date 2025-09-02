c6a040b612dcae4a1ce71b538f3ef57714f0c76d
HyeEun_Lee <blackhaze1024@gmail.com>
2025-09-02 22:41:27 +0900

    feat: make login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,17 @@
+import Button from "../components/ui/button";
+
+export default function LoginPage() {
+  return (
+  <div className="max-w-screen-sm min-h-screen mx-auto px-8 flex flex-col items-center justify-center ">
+    <div>
+    <div className="aspect-[2/3] relative w-full overflow-hidden ">
+    <div className="absolute inset-0 flex flex-col items-center justify-center gap-3">
+      <img src="/logo.png" alt="logo" className="w-[160px] h-auto" />
+      <img src="/title.png" alt="title" className="w-[220px] h-auto" />
+    </div>  
+    </div>
+    <Button className={`bg-[#fee500] w-[220px] flex items-center gap-4 -translate-y-3 text- font-medium`} type="submit" iconPosition="left" icons= {<img src="/kakaotalk.png" alt="kakaotalk logo" className="w-[40px] h-auto" />}>카카오 로그인</Button>
+    </div>
+  </div>
+  )
+}
